9648f13090e54990c5f9430c727afeb74f5cf1d5
Camila Maia <cmaiacd@gmail.com>
2018-06-14 14:13:57 -0300

    Add ship to sceneView

--- ARKitHorizontalPlaneDemo/ViewController.swift
+++ ARKitHorizontalPlaneDemo/ViewController.swift
@@ -7,8 +7,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Uncomment to configure lighting
-        // configureLighting()
+        addTapGestureToSceneView()
+        configureLighting()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -32,6 +32,30 @@ class ViewController: UIViewController {
     func configureLighting() {
         sceneView.autoenablesDefaultLighting = true
         sceneView.automaticallyUpdatesLighting = true
+    }
+
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addShipToSceneView(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    @objc func addShipToSceneView(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+
+        guard let hitTestResult = hitTestResults.first else { return }
+        let translation = hitTestResult.worldTransform.translation
+        let x = translation.x
+        let y = translation.y
+        let z = translation.z
+
+        guard let shipScene = SCNScene(named: "ship.scn"),
+            let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false)
+            else { return }
+
+
+        shipNode.position = SCNVector3(x,y,z)
+        sceneView.scene.rootNode.addChildNode(shipNode)
     }
     
 }
@@ -71,19 +95,16 @@ extension ViewController: ARSCNViewDelegate {
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        // 1
         guard let planeAnchor = anchor as?  ARPlaneAnchor,
             let planeNode = node.childNodes.first,
             let plane = planeNode.geometry as? SCNPlane
             else { return }
 
-        // 2
         let width = CGFloat(planeAnchor.extent.x)
         let height = CGFloat(planeAnchor.extent.z)
         plane.width = width
         plane.height = height
 
-        // 3
         let x = CGFloat(planeAnchor.center.x)
         let y = CGFloat(planeAnchor.center.y)
         let z = CGFloat(planeAnchor.center.z)
